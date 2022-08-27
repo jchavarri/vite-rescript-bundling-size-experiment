3c9d4da7684840ffc8ed5027fe52591fe9403f48
Javier Chávarri <javier.chavarri@gmail.com>
2022-08-27 16:41:44 +0200

    init commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Counter from "../components/Counter";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Counter />
+    </>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import analyze from "rollup-plugin-analyzer";
+
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      plugins: [analyze()],
+    },
+  },
+  plugins: [react()],
+});
